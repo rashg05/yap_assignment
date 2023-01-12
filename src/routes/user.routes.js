82d27ddefc8f6d72146c603c03c24060f5478113
rashg05 <gajra.rashmi0504@gmail.com>
2023-01-12 10:43:51 +0530

    changes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { userController } = require('../controllers');
 
+// eslint-disable-next-line new-cap
 const userRoutes = express.Router({});
 
 userRoutes.post('/', userController.addOne);
